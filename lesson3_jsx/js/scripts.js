3b6690df00b6dced0fdf96b791bfe7fa156f99b4
valera <elBarto2006@ukr.net>
2020-01-05 00:49:21 +0200

    end lesson3

--- lesson3_jsx/js/scripts.js
+++ lesson3_jsx/js/scripts.js
@@ -1,7 +1,51 @@
-let heading = React.createElement(
-    "h1",
-    null,
-    " Hello World! "
-);
 
-ReactDOM.render(heading, document.getElementById("content"));
+/**   Use the value */
+// let heading =  <h1> Hello World! </h1>;
+
+
+// ReactDOM.render(
+//     heading,
+//     document.getElementById("content")
+
+// );
+
+/** Use the class */
+let value = 1;
+let myStyle = { backgroundColor: "black", color: "white" };
+
+class HelloWorld extends React.Component {
+    getUrl() {
+        return "https://google.com";
+    }
+
+    render() {
+        if (value === 1) {
+            let heading = React.createElement(
+                "h1",
+                { title: this.props.title, myName: this.props.myName },
+                " Hello World! "
+            );
+            return React.createElement(
+                "h1",
+                { className: "myNyClass", style: myStyle },
+                "Hellow",
+                this.props.myName,
+                heading,
+                React.createElement(
+                    "a",
+                    { disabled: true, href: this.getUrl() },
+                    "google"
+                ),
+                "World"
+            );
+        } else {
+            return React.createElement(
+                "h1",
+                { style: { color: "red", backgroundColor: "green" } },
+                "value > 1 "
+            );
+        }
+    }
+}
+
+ReactDOM.render(React.createElement(HelloWorld, { title: "HELLO", myName: "Valera" }), document.getElementById("content"));
--- lesson3_jsx/jsx/scripts.jsx
+++ lesson3_jsx/jsx/scripts.jsx
@@ -1,8 +1,45 @@
-let heading =  <h1> Hello World! </h1>;
 
+/**   Use the value */
+// let heading =  <h1> Hello World! </h1>;
+
+
+// ReactDOM.render(
+//     heading,
+//     document.getElementById("content")
+
+// );
+
+/** Use the class */
+let value=1;
+let myStyle={backgroundColor:"black", color:"white"}
+
+
+class HelloWorld extends React.Component{
+    getUrl(){
+        return "https://google.com"
+    }
+
+    render(){
+        if(value === 1){
+            let heading =  <h1 title={this.props.title} myName={this.props.myName}> Hello World! </h1>;
+            return (
+                <h1 className="myNyClass" style={myStyle}>
+                    Hellow 
+                    {this.props.myName} 
+                    {heading} 
+                    <a disabled={true} href={this.getUrl()}>google</a> 
+                    World 
+                </h1>
+                )
+        }else{
+            return( <h1 style={{color:"red", backgroundColor:"green"}}>value > 1 </h1>)
+        }
+       
+    }
+}
 
 ReactDOM.render(
-    heading,
+    <HelloWorld title="HELLO" myName="Valera"/>,
     document.getElementById("content")
 
 );
